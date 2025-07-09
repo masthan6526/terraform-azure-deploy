7a19dea04aa9439e64b67358d51a4f5b6146d41c
masthan6526 <147030557+masthan6526@users.noreply.github.com>
2025-07-09 14:36:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 }
 
 # Virtual Network
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network1" "vnet" {
   name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
@@ -24,7 +24,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "public" {
   name                 = "public-subnet"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network1.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
@@ -32,7 +32,7 @@ resource "azurerm_subnet" "public" {
 resource "azurerm_subnet" "private" {
   name                 = "private-subnet"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
