d859af37cf1cc2d621fa66452aa9ede07fbbbda6
masthan6526 <147030557+masthan6526@users.noreply.github.com>
2025-07-09 13:15:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group_name" {
   description = "Name of the existing Azure Resource Group"
   type        = string
-  default     = "azurerm_virtual_network"
+  default     = "example-resources"
 }
